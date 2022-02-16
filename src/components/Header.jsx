4b66dd07edadb70928d7770fcfbedab4fdaea146
Your Name <you@example.com>
2022-02-16 21:30:07 +0200

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://w7.pngwing.com/pngs/436/701/png-transparent-lay-s-frito-lay-logo-fritos-potato-chip-others.png' />
+        </header>)
+
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+            </div>
+            <div>
+                ava+description
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg" />
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 1
+                </div>
+            </div>
+        </div>)
+
+}
+
+export default Profile;
